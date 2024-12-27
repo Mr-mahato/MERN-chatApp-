068aef869cefce8c8661593c936f0690a9d474ee
Mr-mahato <cmhandan@gmail.com>
2024-12-27 23:25:31 +0530

    added few endpoints to the backend for the socket

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const { Server } = require("socket.io");
 const { createServer } = require("http");
 const app = express();
 const server = createServer(app);
-
+const rooms = new Set();
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
@@ -21,11 +21,32 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("Disconnected: ", socket.id);
   });
-  // user sending some message to the socket
+  // user sending some message to particular room to the socket
   socket.on("userMessage", (mssg) => {
     console.log(mssg);
-    io.emit("chat_message", mssg);
+    io.to(mssg.roomId).emit("chat_message", mssg);
   });
+
+  // lets add event for adding the rooms here
+  socket.on("joinRoom", (room) => {
+    console.log(room);
+    socket.join(room);
+    rooms.add(room);
+    io.emit("activeRooms", Array.from(rooms));
+  });
+
+  socket.on("allRooms", () => {
+    console.log(rooms);
+    console.log(rooms.size);
+    socket.emit("activeRooms", Array.from(rooms));
+  });
+
+  socket.on("getConnectedSockets", () => {
+    const connectedSockets = Array.from(io.sockets.sockets.keys());
+    socket.emit("connectedSockets", connectedSockets);
+  });
+
+  // work on single individual user
 });
 
 server.listen(3001, (err) => {
