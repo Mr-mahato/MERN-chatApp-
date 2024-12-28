997fcf8218120bbd88b585001e6e5fccc523d2dd
Mr-mahato <cmhandan@gmail.com>
2024-12-28 21:09:42 +0530

    updated bacekdn

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const { Server } = require("socket.io");
+const dotenv = require("dotenv");
 const { createServer } = require("http");
+dotenv.config();
+const connectDB = require("./config/dbConn");
+const User = require("./Model/UserSchema");
+const Message = require("./Model/MessageSchema");
+const Room = require("./Model/RoomsSchema");
+const authRouter = require("./Routes/userRegistration");
 const app = express();
+app.use(cors());
 const server = createServer(app);
-const rooms = new Set();
+app.use(express.json());
+app.use("/api/v1", authRouter);
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:5173",
@@ -15,41 +24,113 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+const users = new Map();
+
 io.on("connection", (socket) => {
   console.log("Connection established", socket.id);
-  // socket disconnection time
+
+  // Handle user authentication
+  socket.on("authenticate", async (userId) => {
+    const user = await User.findById(userId); // after getting the user
+    if (user) {
+      users.set(user.username, { username: user.username, socketId: socket.id });
+
+      io.emit("online users", Array.from(users.values()));
+    } else {
+      socket.disconnect();
+    }
+  });
+
+  // Handle user disconnection
   socket.on("disconnect", () => {
-    console.log("Disconnected: ", socket.id);
-  });
-  // user sending some message to particular room to the socket
-  socket.on("userMessage", (mssg) => {
-    console.log(mssg);
-    io.to(mssg.roomId).emit("chat_message", mssg);
-  });
-
-  // lets add event for adding the rooms here
-  socket.on("joinRoom", (room) => {
-    console.log(room);
-    socket.join(room);
-    rooms.add(room);
-    io.emit("activeRooms", Array.from(rooms));
+    users.forEach((value, key) => {
+      if (value.socketId === socket.id) {
+        users.delete(key);
+      }
+    });
+    io.emit("online users", Array.from(users.values()));
   });
 
-  socket.on("allRooms", () => {
-    console.log(rooms);
-    console.log(rooms.size);
-    socket.emit("activeRooms", Array.from(rooms));
+  // Handle user messages within a room
+  socket.on("send message", async ({ payload, isChannel, chatName }) => {
+    try {
+      if (isChannel) {
+        const message = new Message({
+          content: payload.content,
+          userId: payload.userId,
+          roomId: payload.roomId,
+          username: payload.username,
+          timestamp: payload.time,
+        });
+        // await message.save();
+        io.to(chatName).emit("new message", payload);
+      } else {
+        console.log(payload);
+
+        // here the chatName is gonna be the socket id of the person who i want to send the messages.
+        io.to(chatName).emit("new message", payload);
+      }
+    } catch (error) {
+      console.error("Error saving message:", error);
+      socket.emit("error_saving_message", { error: "Failed to save message" });
+    }
   });
 
+  // sender -> name , receiver->name
+  socket.on("private message", ({ sender, receiver, message }) => {
+    let receiverInfo = users[receiver];
+    console.log(sender , receiver , message)
+    users.forEach((val , ind)=>{
+      if(val.username === receiver){
+        receiverInfo = val;
+      }
+    })
+    console.log(receiverInfo);
+    console.log(receiverInfo);
+    if (receiverInfo) {
+      io.to(receiverInfo.socketId).emit("private_message", { sender, message });
+    }
+    // Send the message back to the sender as well
+    socket.emit("private_message", { sender, message });
+  });
+
+  // Handle room joining / creation of the room when a particular socket joins the room
+  socket.on("joinRoom", async (roomDetail) => {
+    const { roomName, owner } = roomDetail;
+    socket.join(roomName);
+
+    let room = await Room.findOne({ name: roomName });
+    if (!room) {
+      room = new Room({ name: roomName, owner });
+      await room.save();
+    }
+    io.emit("activeRooms", await Room.find().select("name"));
+  });
+
+  // Handle request to get all the rooms we have
+  socket.on("allRooms", async () => {
+    const rooms = await Room.find().select("name");
+    socket.emit("activeRooms", rooms);
+  });
+
+  // Handle request for connected sockets or those who are online
   socket.on("getConnectedSockets", () => {
-    const connectedSockets = Array.from(io.sockets.sockets.keys());
-    socket.emit("connectedSockets", connectedSockets);
+    socket.emit("connectedSockets", Array.from(io.sockets.sockets.keys()));
   });
 
-  // work on single individual user
+  // Handle request for room information
+  socket.on("getRoomInfo", async (roomName) => {
+    const room = await Room.findOne({ name: roomName });
+    if (room) {
+      socket.emit("roomInfo", room);
+    } else {
+      socket.emit("roomInfo", { error: "Room not found" });
+    }
+  });
 });
 
-server.listen(3001, (err) => {
+server.listen(3001, async (err) => {
   if (err) console.log("Error found", err);
-  console.log("Your server is running like usain bolt....");
+  await connectDB();
+  console.log("Your server is running like Usain Bolt....");
 });
