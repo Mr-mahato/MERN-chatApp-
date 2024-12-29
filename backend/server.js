5d384ac8c87fef26a8cb8e5bd82bf33fb0b58abe
Mr-mahato <cmhandan@gmail.com>
2024-12-29 23:42:02 +0530

    added the frontend deployed url

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use("/api/v1",chatRouter)
 app.use('/api/v1',directMessageRouter);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173" , "https://letschatnp.vercel.app/"],
   },
 });
 
